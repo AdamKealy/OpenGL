54e5f0601e33fa93fd83dac982a2415aa1cb1ad9
c00213914 <c00213914@A310C034>
2017-12-07 12:54:51 +0000

    part 1 done?

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -33,6 +33,11 @@ void Game::run()
 void Game::initialize()
 {
 	isRunning = true;
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
+	glMatrixMode(GL_MODELVIEW);
 }
 
 void Game::update()
@@ -43,6 +48,67 @@ void Game::update()
 void Game::draw()
 {
 	cout << "Draw up" << endl;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+	//glBegin(GL_POINTS);
+	//{glVertex3f(0.0,2.0,-5.0); }
+	//glEnd();
+
+	/*glBegin(GL_LINES);
+	{glVertex3f(0.0, 2.0, -5.0); glVertex3f(0.0, -2.0, -5.0); }
+	glEnd();*/
+
+	/*glBegin(GL_LINE_STRIP);
+	{glVertex3f(0.0, 2.0, -5.0); glVertex3f(0.0, -2.0, -5.0); glVertex3f(2.0, 0.0, -5.0); }
+	glEnd();*/
+
+	/*glBegin(GL_LINE_LOOP);
+	{glVertex3f(0.0, 2.0, -5.0); glVertex3f(0.0, -2.0, -5.0); glVertex3f(2.0, 0.0, -5.0); }
+	glEnd();*/
+
+	/*glBegin(GL_LINE_LOOP);
+	{glVertex3f(0.0, 2.0, -5.0); glVertex3f(0.0, -2.0, -5.0); glVertex3f(2.0, 0.0, -5.0); }
+	glEnd();*/
+
+	//glBegin(GL_TRIANGLES);
+	//{ glVertex3f(0.0, 2.0, -5.0); glVertex3f(-2.0, -2.0, -5.0); glVertex3f(2.0, -2.0, -5.0); }
+	//glEnd();
+
+	/*glBegin(GL_TRIANGLE_STRIP);
+	{glVertex3f(0.0, 2.0, -5.0); glVertex3f(2.0, -2.0, -5.0); glVertex3f(2.0, 2.0, -5.0); glVertex3f(-2.0, -2.0, -5.0); }
+	glEnd();*/
+
+	//glBegin(GL_TRIANGLE_FAN);
+	//{glVertex3f(0.0, 2.0, -5.0); glVertex3f(2.0, -2.0, -5.0); glVertex3f(2.0, 0.0, -5.0); glVertex3f(0.0, -2.0, -5.0); glVertex3f(-2.0, 0.0, -5.0); glVertex3f(.0, 2.0, -5.0); }
+	//glEnd();
+
+	/*glBegin(GL_QUADS);
+	{glVertex3f(-2.0, 2.0, -5.0); glVertex3f(-2.0, -2.0, -5.0); glVertex3f(2.0, -2.0, -5.0); glVertex3f(2.0, 2.0, -5.0);}
+	glEnd();*/
+
+	//glBegin(GL_QUAD_STRIP);
+	//{glVertex3f(-2.0, 2.0, -5.0); glVertex3f(-1.0, -1.0, -5.0); glVertex3f(0.0, 0.0, -5.0); glVertex3f(-1.0,0.0,-5.0); }
+	//glEnd();
+
+	//glBegin(GL_QUAD_STRIP);
+	//{glVertex3f(2.0, 2.0, -5.0); glVertex3f(1.0, -1.0, -5.0); glVertex3f(0.0, 0.0, -5.0); glVertex3f(1.0, 0.0, -5.0); }
+	//glEnd();
+
+	//glBegin(GL_QUAD_STRIP);
+	//{glVertex3f(-2.0, -2.0, -5.0); glVertex3f(-1.0, -1.0, -5.0); glVertex3f(0.0, -1.0, -5.0); glVertex3f(-1.0, -1.0, -5.0); }
+	//glEnd();
+
+	//glBegin(GL_QUAD_STRIP);
+	//{glVertex3f(2.0, -2.0, -5.0); glVertex3f(1.0, -1.0, -5.0); glVertex3f(0.0, -1.0, -5.0); glVertex3f(1.0, -1.0, -5.0); }
+	//glEnd();
+	
+	glBegin(GL_POLYGON);
+	{glVertex3f(-2.0, 2.0, -5.0); glVertex3f(-2.0, -2.0, -5.0); glVertex3f(2.0, -2.0, -5.0); glVertex3f(2.0, 2.0, -5.0); }
+	glEnd();
+
+	window.display();
+
 }
 
 void Game::unload()
